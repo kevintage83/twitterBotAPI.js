a235e9c86da29b5e27b3a6d83b0c4778371f7abf
kevin <kevintage83@users.noreply.github.com>
2018-02-02 15:37:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 //Put your keys in this module.
 module.exports = {
-
-
+    consumer_key:        '...'
+  , consumer_secret:     '...'
+  , access_token:        '...'
+  , access_token_secret: '...'
 }

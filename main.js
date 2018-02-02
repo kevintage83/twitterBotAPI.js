694bc0c3e191753da4b008b6fbe890317a7fb4ec
kevin <kevintage83@users.noreply.github.com>
2018-02-02 15:40:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ console.log("The bot is starting");
 //This is an import statement.
 var twit = require('twit');
 
-var config = require('config');
+var config = require('./config');
 
-var T = new Twit({
-});
+var T = new Twit(config);
 
 //Tweet "Hello, world!"
+15.4 Twitter API Basics - Twitter bot is next...

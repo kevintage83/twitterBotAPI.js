31a29736e71bded67f018d5d268c39e137b260f7
kevin <kevintage83@users.noreply.github.com>
2018-02-02 15:35:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+//This is a console indicator that the file started executing.
+console.log("The bot is starting");
 
+//This is an import statement.
+var twit = require('twit');
+
+var T = new Twit({
+    consumer_key:        '...'
+  , consumer_secret:     '...'
+  , access_token:        '...'
+  , access_token_secret: '...'
+});
+
+//Tweet "Hello, world!"
